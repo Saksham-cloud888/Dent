5304f744582d1ab16cde4773840f60115793cfcf
Saksham-cloud888 <saksham.pdl443@gmail.com>
2025-11-18 00:50:57 +0545

    Update connection.js

--- server/lib/connection.js
+++ server/lib/connection.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 
-/**
- * Function that connects to the mongodb database
- */
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://sakshampdl443_db_user:%3C%23Saksham%40123%3E@cluster0.rqdbgfy.mongodb.net/?appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
+
